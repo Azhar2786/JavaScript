5b8a78b3b5a34bcb620c8657134541f9b36f28a4
Azhar <darkcoders0101@gmail.com>
2024-01-17 00:15:18 +0530

    Discussion on Variables

---
+++ Basics_1/01_variables.js
@@ -0,0 +1,13 @@
+
+
+const accountId = 12334;
+let accountEmail = "azhar@google.com";
+var accountPassword = "12323232";
+
+accountCity = "Jaipur";
+
+// accountId = 2; // not allowed
+
+// console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
