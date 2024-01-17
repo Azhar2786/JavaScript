20ef214173f243d4ebd452259945f4317632eb75
Azhar <darkcoders0101@gmail.com>
2024-01-17 16:20:35 +0530

    Discussion on Comparision operator

---
+++ Basics_1/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 < 1);
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > -1);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+/*
+the above code output true, false, true, true:
+because equality check == and comparisions > < >= <= work differently
+comparision convert null to a number, treating it as 0.
+*/
+
