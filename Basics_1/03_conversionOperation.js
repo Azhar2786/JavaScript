41df45fc3adc5920da28ebba10a643a5eb54b530
Azhar <darkcoders0101@gmail.com>
2024-01-17 16:21:04 +0530

    Discussion on conversion operation

--- Basics_1/03_conversionOperation.js
+++ Basics_1/03_conversionOperation.js
@@ -41,28 +41,29 @@ let negValue = -value
 // console.log(2%3);
 
 let str1 = "hello"
-let str2 = " hitesh"
+let str2 = " azhar"
 
 let str3 = str1 + str2
-// console.log(str3);
+console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
 
 // console.log( (3 + 4) * 5 % 3);
 
 // console.log(+true);
 // console.log(+"");
 
-let num1, num2, num3
+// let num1, num2, num3
 
-num1 = num2 = num3 = 2 + 2
+// num1 = num2 = num3 = 2 + 2
 
-let gameCounter = 100
-++gameCounter;
-console.log(gameCounter);
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
